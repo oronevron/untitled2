10620437ede7155bcfea3f0c5da22b9ff6c08809
barel <barel31@gmail.com>
2017-03-08 19:04:49 +0200

    add image read with tf

--- barel_tensor_black.py
+++ barel_tensor_black.py
@@ -5,7 +5,7 @@ import math
 import os.path
 import random
 from os import listdir
-
+import globalFunctions
 
 from tensorflow.examples.tutorials.mnist import input_data
 import matplotlib.pyplot as plt
@@ -13,149 +13,153 @@ import matplotlib.pyplot as plt
 # load image with tensorflow as rgb image. and load the picture as tensor at the start the work.
 # do 20 epoks - the best one was epok 9 with 77.0% accuracy. test data %77.6
 
-def load_image():
-    train_image = []
-    train_labal = []
-    validation_image = []
-    validation_labal = []
-    test_image = []
-    test_labal = []
+# def load_image():
+#     train_image = []
+#     train_labal = []
+#     validation_image = []
+#     validation_labal = []
+#     test_image = []
+#     test_labal = []
+#
+#     # For every label
+#     for i in range(0,50):
+#
+#         image_list = []
+#         labal_hot = []
+#
+#         # Init lavel one hot vector
+#         labal = np.zeros(50)
+#         labal[i] = 1
+#
+#         if (i==22):
+#             continue
+#
+#         file_list = listdir('FinalDataset/jpg/' + str(i))
+#
+#
+#         for j in range(0,len(file_list)):
+#             file_list[j] = 'FinalDataset/jpg/' + str(i) + '/' + file_list[j]
+#
+#         filename_queue = tf.train.string_input_producer(file_list)  # list of files to read
+#
+#         reader = tf.WholeFileReader()
+#         key, value = reader.read(filename_queue)
+#
+#         my_img = tf.image.decode_jpeg(value, channels=1)  # use png or jpg decoder based on your files.
+#         #my_img2 = tf.image.resize_image_with_crop_or_pad(value, 40,40)
+#         my_img_re = tf.image.resize_images(my_img, [40,40])
+#         my_img_re = tf.image.random_flip_up_down(my_img_re, seed=None)
+#         init_op = tf.initialize_all_variables()
+#
+#         with tf.Session() as sess:
+#             sess.run(init_op)
+#
+#             # Start populating the filename queue.
+#             coord = tf.train.Coordinator()
+#             threads = tf.train.start_queue_runners(coord=coord)
+#
+#             for m in range(0,len(file_list)):  # length of your filename list
+#                 #print(str(i) + "," + file_list[m])
+#                 image = my_img_re.eval()  # here is your image Tensor :)
+#                 resized_image = np.asarray(image)
+#                # resized_image = resized_image.transpose(2,0,1).reshape(3,-1)
+#                 #resized_image = resized_image.transpose(1,0)
+#                 #resized_image = resized_image.squeeze()
+#                # resized_image = resized_image.flatten()
+#                 resized_image = resized_image / 255.
+#
+#                 image_list.append(resized_image)
+#                 labal_hot.append(labal)
+#
+#             coord.request_stop()
+#             coord.join(threads)
+#
+#         # Shuffle data.
+#         random.shuffle(image_list)
+#
+#         # Calc size of each group(train-validation-test)
+#         image_number = len(image_list)
+#         test_image_size = int(math.ceil(image_number * 0.84))
+#         validation_image_size = int(math.ceil(image_number * 0.92))
+#
+#         # Split current label image to train-validation-test
+#         train_image.extend(image_list[:test_image_size])
+#         train_labal.extend(labal_hot[:test_image_size])
+#         validation_image.extend(image_list[test_image_size:validation_image_size])
+#         validation_labal.extend(labal_hot[test_image_size:validation_image_size])
+#         test_image.extend(image_list[validation_image_size:])
+#         test_labal.extend(labal_hot[validation_image_size:])
+#
+#     # list to numpy array.
+#     train_image = np.array(train_image)
+#     train_labal = np.array(train_labal)
+#     validation_image = np.array(validation_image)
+#     validation_labal = np.array(validation_labal)
+#     test_image = np.array(test_image)
+#     test_labal = np.array(test_labal)
+#
+#     # Shuffle data.
+#     train_image, train_labal = Shuffle(train_image, train_labal)
+#     validation_image, validation_labal = Shuffle(validation_image, validation_labal)
+#     test_image, test_labal = Shuffle(test_image, test_labal)
+#
+#     # Save image.
+#     #save_to_file(train_image, train_labal, validation_image, validation_labal, test_image, test_labal)
+#
+#     return train_image, train_labal, validation_image, validation_labal, test_image, test_labal
+#
+# # Load image from binary file.
+# def load_from_file():
+#
+#     if os.path.exists('FinalDataset/bin/traikn_image.npy') is True:
+#         train_image = np.load('FinalDataset/bin/train_image.npy')
+#         train_labal = np.load('FinalDataset/bin/train_labal.npy')
+#         validation_image = np.load('FinalDataset/bin/validation_image.npy')
+#         validation_labal = np.load('FinalDataset/bin/validation_labal.npy')
+#         test_image = np.load('FinalDataset/bin/test_image.npy')
+#         test_labal = np.load('FinalDataset/bin/test_labal.npy')
+#     else:
+#         train_image, train_labal, validation_image, validation_labal, test_image, test_labal = load_image()
+#
+#     # Return data.
+#     return train_image, train_labal, validation_image, validation_labal, test_image, test_labal
+#
+# # Save image to binary file.
+# def save_to_file(train_image, train_labal, validation_image, validation_labal, test_image, test_labal):
+#       np.save('FinalDataset/bin/train_image.npy',train_image)
+#       np.save('FinalDataset/bin/train_labal.npy',train_labal)
+#       np.save('FinalDataset/bin/validation_image.npy',validation_image)
+#       np.save('FinalDataset/bin/validation_labal.npy', validation_labal)
+#       np.save('FinalDataset/bin/test_image.npy',test_image)
+#       np.save('FinalDataset/bin/test_labal.npy',test_labal)
+#
+# def Shuffle(Image, Label):
+#     perm = np.arange(len(Image))
+#     np.random.shuffle(perm)
+#     Image = Image[perm]
+#     Label = Label[perm]
+#
+#     return Image, Label
+# def conv2d(x, W):
+#   return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+#
+# def max_pool_2x2(x):
+#   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+#                         strides=[1, 2, 2, 1], padding='SAME')
+#
+# # functions for parameter initialization
+# def weight_variable(shape):
+#   initial = tf.truncated_normal(shape, stddev=0.1)
+#   return tf.Variable(initial)
+#
+# def bias_variable(shape):
+#   initial = tf.constant(0.1, shape=shape)
+#   return tf.Variable(initial)
 
-    # For every label
-    for i in range(0,50):
-
-        image_list = []
-        labal_hot = []
-
-        # Init lavel one hot vector
-        labal = np.zeros(50)
-        labal[i] = 1
-
-        file_list = listdir('FinalDataset/jpg/' + str(i))
-        if len(file_list) == 0:
-            continue
-
-        for j in range(0,len(file_list)):
-            file_list[j] = 'FinalDataset/jpg/' + str(i) + '/' + file_list[j]
-
-        filename_queue = tf.train.string_input_producer(file_list)  # list of files to read
-
-        reader = tf.WholeFileReader()
-        key, value = reader.read(filename_queue)
-
-        my_img = tf.image.decode_jpeg(value, channels=1)  # use png or jpg decoder based on your files.
-        my_img_re = tf.image.resize_images(my_img, [40, 40])
-
-        init_op = tf.initialize_all_variables()
-
-        with tf.Session() as sess:
-            sess.run(init_op)
-
-            # Start populating the filename queue.
-            coord = tf.train.Coordinator()
-            threads = tf.train.start_queue_runners(coord=coord)
-
-            for m in range(0,len(file_list)):  # length of your filename list
-                #print(str(i) + "," + file_list[m])
-                image = my_img_re.eval()  # here is your image Tensor :)
-                resized_image = np.asarray(image)
-               # resized_image = resized_image.transpose(2,0,1).reshape(3,-1)
-                #resized_image = resized_image.transpose(1,0)
-                #resized_image = resized_image.squeeze()
-               # resized_image = resized_image.flatten()
-                resized_image = resized_image / 255.
-
-                image_list.append(resized_image)
-                labal_hot.append(labal)
-
-            coord.request_stop()
-            coord.join(threads)
-
-        # Shuffle data.
-        random.shuffle(image_list)
-
-        # Calc size of each group(train-validation-test)
-        image_number = len(image_list)
-        test_image_size = int(math.ceil(image_number * 0.84))
-        validation_image_size = int(math.ceil(image_number * 0.92))
-
-        # Split current label image to train-validation-test
-        train_image.extend(image_list[:test_image_size])
-        train_labal.extend(labal_hot[:test_image_size])
-        validation_image.extend(image_list[test_image_size:validation_image_size])
-        validation_labal.extend(labal_hot[test_image_size:validation_image_size])
-        test_image.extend(image_list[validation_image_size:])
-        test_labal.extend(labal_hot[validation_image_size:])
-
-    # list to numpy array.
-    train_image = np.array(train_image)
-    train_labal = np.array(train_labal)
-    validation_image = np.array(validation_image)
-    validation_labal = np.array(validation_labal)
-    test_image = np.array(test_image)
-    test_labal = np.array(test_labal)
-
-    # Shuffle data.
-    train_image, train_labal = Shuffle(train_image, train_labal)
-    validation_image, validation_labal = Shuffle(validation_image, validation_labal)
-    test_image, test_labal = Shuffle(test_image, test_labal)
-
-    # Save image.
-    save_to_file(train_image, train_labal, validation_image, validation_labal, test_image, test_labal)
-
-    return train_image, train_labal, validation_image, validation_labal, test_image, test_labal
-
-# Load image from binary file.
-def load_from_file():
-
-    if os.path.exists('FinalDataset/bin/traikn_image.npy') is True:
-        train_image = np.load('FinalDataset/bin/train_image.npy')
-        train_labal = np.load('FinalDataset/bin/train_labal.npy')
-        validation_image = np.load('FinalDataset/bin/validation_image.npy')
-        validation_labal = np.load('FinalDataset/bin/validation_labal.npy')
-        test_image = np.load('FinalDataset/bin/test_image.npy')
-        test_labal = np.load('FinalDataset/bin/test_labal.npy')
-    else:
-        train_image, train_labal, validation_image, validation_labal, test_image, test_labal = load_image()
-
-    # Return data.
-    return train_image, train_labal, validation_image, validation_labal, test_image, test_labal
-
-# Save image to binary file.
-def save_to_file(train_image, train_labal, validation_image, validation_labal, test_image, test_labal):
-      np.save('FinalDataset/bin/train_image.npy',train_image)
-      np.save('FinalDataset/bin/train_labal.npy',train_labal)
-      np.save('FinalDataset/bin/validation_image.npy',validation_image)
-      np.save('FinalDataset/bin/validation_labal.npy', validation_labal)
-      np.save('FinalDataset/bin/test_image.npy',test_image)
-      np.save('FinalDataset/bin/test_labal.npy',test_labal)
-
-def Shuffle(Image, Label):
-    perm = np.arange(len(Image))
-    np.random.shuffle(perm)
-    Image = Image[perm]
-    Label = Label[perm]
-
-    return Image, Label
-def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-def max_pool_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
-
-# functions for parameter initialization
-def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
-
-def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
-8
 
 # Maim code start here.
-train_image,train_labal,validation_image,validation_labal,test_image,test_labal = load_from_file()
+train_image,train_labal,validation_image,validation_labal,test_image,test_labal \
+    = globalFunctions.load_from_file( 0, 'TF', 40, True, force_load=True)
 
 # We start
 #n_input = 1600
@@ -174,22 +178,22 @@ x_tensor2 = x2
 # Bias is [output_channels]
 filter_size = 5
 n_filters_1 = 16
-n_filters_2 = 16
+n_filters_2 = 32
 
 # parameters
-W_conv1 = weight_variable([filter_size, filter_size, 1, n_filters_1])
-b_conv1 = bias_variable([n_filters_1])
-W_conv2 = weight_variable([filter_size, filter_size, n_filters_1, n_filters_2])
-b_conv2 = bias_variable([n_filters_2])
-W_conv3 = weight_variable([filter_size, filter_size, n_filters_1, n_filters_2])
-b_conv3 = bias_variable([n_filters_2])
+W_conv1 = globalFunctions.weight_variable([filter_size, filter_size, 1, n_filters_1])
+b_conv1 = globalFunctions.bias_variable([n_filters_1])
+W_conv2 = globalFunctions.weight_variable([filter_size, filter_size, n_filters_1, n_filters_2])
+b_conv2 = globalFunctions.bias_variable([n_filters_2])
+W_conv3 = globalFunctions.weight_variable([filter_size, filter_size, n_filters_1, n_filters_2])
+b_conv3 = globalFunctions.bias_variable([n_filters_2])
 
 # layers
-h_conv1 = tf.nn.relu(conv2d(x_tensor2, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+h_conv1 = tf.nn.relu(globalFunctions.conv2d(x_tensor2, W_conv1) + b_conv1)
+h_pool1 = globalFunctions.max_pool_2x2(h_conv1)
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+h_conv2 = tf.nn.relu(globalFunctions.conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = globalFunctions.max_pool_2x2(h_conv2)
 
 
 # 7x7 is the size of the image after the convolutional and pooling layers (28x28 -> 14x14 -> 7x7)
@@ -197,16 +201,16 @@ h_conv2_flat = tf.reshape(h_pool2, [-1, 10 * 10 * n_filters_2])
 
 
 # %% Create the one fully-connected layer:
-n_fc = 740
-W_fc1 = weight_variable([10 * 10 * n_filters_2, n_fc])
-b_fc1 = bias_variable([n_fc])
+n_fc = 1200
+W_fc1 = globalFunctions.weight_variable([10 * 10 * n_filters_2, n_fc])
+b_fc1 = globalFunctions.bias_variable([n_fc])
 h_fc1 = tf.nn.relu(tf.matmul(h_conv2_flat, W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-W_fc2 = weight_variable([n_fc, n_output])
-b_fc2 = bias_variable([n_output])
+W_fc2 = globalFunctions.weight_variable([n_fc, n_output])
+b_fc2 = globalFunctions.bias_variable([n_output])
 y_pred = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 cross_entropy = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(y_pred, y))
@@ -244,7 +248,13 @@ for epoch_i in range(n_epochs):
     l_loss.append(loss)
 
     # Shuffle data.
-    train_image, train_labal = Shuffle(train_image, train_labal)
+    train_image, train_labal = globalFunctions.Shuffle(train_image, train_labal)
+
+    prediction = tf.argmax(y_pred, 1)
+    labels = prediction.eval(feed_dict={x2: validation_image, y: validation_labal, keep_prob: 1.0}, session=sess)
+
+   # confusion_matrix1(one_hot_to_label_array(validation_labal), predictions[0])
+    globalFunctions.confusion_matrix1(globalFunctions.one_hot_to_label_array(validation_labal), labels)
 
 print("Accuracy for test set: {}".format(sess.run(accuracy,
                feed_dict={
--- globalFunctions.py
+++ globalFunctions.py
@@ -4,11 +4,15 @@ import cv2
 import math
 import os.path
 import random
+from os import listdir
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
+import pandas as pd
 
 from tensorflow.examples.tutorials.mnist import input_data
 import matplotlib.pyplot as plt
 
-def load_image():
+def load_image(image_type, load_type, size, with_rot):
     train_image = []
     train_labal = []
     validation_image = []
@@ -16,38 +20,94 @@ def load_image():
     test_image = []
     test_labal = []
 
+    dir_list = listdir('FinalDataset/jpg')
+
     # For every label
-    for i in range(0,50):
+    for i in range(0,len(dir_list)):
 
         image_list = []
         labal_hot = []
 
         # Init lavel one hot vector
-        labal = np.zeros(50)
-        labal[i] = 1
+        labal = np.zeros(51)
+        labal[int(dir_list[i])] = 1
 
-        # For every image
-        for j in range(0,1000):
-            # Read image.
-            img = cv2.imread('FinalDataset/jpg/' + str(i) + '/' + str(j) + '.jpg', 0)
-            if (img is None):
-                continue
+        dir_images_list = listdir('FinalDataset/jpg/' + dir_list[i])
 
-            # Resize image.
-            resized_image = cv2.resize(img, (40, 40))
-            resized_image = resized_image.flatten()
-            resized_image = resized_image / 255.
+        if load_type == 'CV':
+            # For every image
+            for j in range(0,len(dir_images_list)):
+                # Read image.
+                img = cv2.imread('FinalDataset/jpg/' + str(dir_list[i]) + '/' + str(dir_images_list[j]),image_type)
+                if (img is None):
+                    continue
 
-            image_list.append(resized_image)
-            labal_hot.append(labal)
+                # Resize image.
+                resized_image = cv2.resize(img, (size, size))
+                #resized_image = resized_image.flatten()
+                resized_image = resized_image / 255.
+
+                image_list.append(resized_image)
+                labal_hot.append(labal)
+        else:
+            for j in range(0, len(dir_images_list)):
+                dir_images_list[j] = 'FinalDataset/jpg/' + str(dir_list[i]) + '/' + dir_images_list[j]
+
+            filename_queue = tf.train.string_input_producer(dir_images_list)  # list of files to read
+
+            reader = tf.WholeFileReader()
+            key, value = reader.read(filename_queue)
+
+            if image_type == 0:
+                channels = 1
+            else:
+                channels = 3
+
+            my_img = tf.image.decode_jpeg(value, channels=channels)  # use png or jpg decoder based on your files.
+            my_img_re = tf.image.resize_images(my_img, [size, size])
+
+            # Rotate iamge 180 degree
+            my_img_re2 = tf.image.rot90(my_img_re, 2, name=None)
+
+            init_op = tf.initialize_all_variables()
+
+            with tf.Session() as sess:
+                sess.run(init_op)
+
+                # Start populating the filename queue.
+                coord = tf.train.Coordinator()
+                threads = tf.train.start_queue_runners(coord=coord)
+
+                for m in range(0, len(dir_images_list)):  # length of your filename list
+                    # print(str(i) + "," + file_list[m])
+                    image = my_img_re.eval()  # here is your image Tensor :)
+                    resized_image = np.asarray(image)
+                    # resized_image = resized_image.transpose(2,0,1).reshape(3,-1)
+                    # resized_image = resized_image.transpose(1,0)
+                    # resized_image = resized_image.squeeze()
+                    # resized_image = resized_image.flatten()
+                    resized_image = resized_image / 255.
+
+                    image_list.append(resized_image)
+                    labal_hot.append(labal)
+
+                    if with_rot == True:
+                        image = my_img_re2.eval()
+                        resized_image = np.asarray(image)
+                        resized_image = resized_image / 255.
+                        image_list.append(resized_image)
+                        labal_hot.append(labal)
+
+                coord.request_stop()
+                coord.join(threads)
 
         # Shuffle data.
-        random.shuffle(image_list)
+        #random.shuffle(image_list)
 
         # Calc size of each group(train-validation-test)
         image_number = len(image_list)
-        test_image_size = int(math.ceil(image_number * 0.8))
-        validation_image_size = int(math.ceil(image_number * 0.9))
+        test_image_size = int(math.ceil(image_number * 0.84))
+        validation_image_size = int(math.ceil(image_number * 0.92))
 
         # Split current label image to train-validation-test
         train_image.extend(image_list[:test_image_size])
@@ -76,9 +136,9 @@ def load_image():
     return train_image,train_labal,validation_image,validation_labal,test_image,test_labal
 
 # Load image from binary file.
-def load_from_file():
+def load_from_file(image_type, load_type, size, with_rot,force_load=False):
 
-    if os.path.exists('FinalDataset/bin/train_image.npy') is True:
+    if force_load == False and os.path.exists('FinalDataset/bin/train_image.npy') is True:
         train_image = np.load('FinalDataset/bin/train_image.npy')
         train_labal = np.load('FinalDataset/bin/train_labal.npy')
         validation_image = np.load('FinalDataset/bin/validation_image.npy')
@@ -86,10 +146,93 @@ def load_from_file():
         test_image = np.load('FinalDataset/bin/test_image.npy')
         test_labal = np.load('FinalDataset/bin/test_labal.npy')
     else:
-        train_image, train_labal, validation_image, validation_labal, test_image, test_labal = load_image()
+        train_image, train_labal, validation_image, validation_labal, test_image, test_labal \
+            = load_image(image_type, load_type, size, with_rot)
 
     # Return data.
     return train_image, train_labal, validation_image, validation_labal, test_image, test_labal
+# def load_image():
+#     train_image = []
+#     train_labal = []
+#     validation_image = []
+#     validation_labal = []
+#     test_image = []
+#     test_labal = []
+#
+#     # For every label
+#     for i in range(0,50):
+#
+#         image_list = []
+#         labal_hot = []
+#
+#         # Init lavel one hot vector
+#         labal = np.zeros(50)
+#         labal[i] = 1
+#
+#         # For every image
+#         for j in range(0,1000):
+#             # Read image.
+#             img = cv2.imread('FinalDataset/jpg/' + str(i) + '/' + str(j) + '.jpg', 0)
+#             if (img is None):
+#                 continue
+#
+#             # Resize image.
+#             resized_image = cv2.resize(img, (40, 40))
+#             resized_image = resized_image.flatten()
+#             resized_image = resized_image / 255.
+#
+#             image_list.append(resized_image)
+#             labal_hot.append(labal)
+#
+#         # Shuffle data.
+#         random.shuffle(image_list)
+#
+#         # Calc size of each group(train-validation-test)
+#         image_number = len(image_list)
+#         test_image_size = int(math.ceil(image_number * 0.8))
+#         validation_image_size = int(math.ceil(image_number * 0.9))
+#
+#         # Split current label image to train-validation-test
+#         train_image.extend(image_list[:test_image_size])
+#         train_labal.extend(labal_hot[:test_image_size])
+#         validation_image.extend(image_list[test_image_size:validation_image_size])
+#         validation_labal.extend(labal_hot[test_image_size:validation_image_size])
+#         test_image.extend(image_list[validation_image_size:])
+#         test_labal.extend(labal_hot[validation_image_size:])
+#
+#     # list to numpy array.
+#     train_image = np.array(train_image)
+#     train_labal = np.array(train_labal)
+#     validation_image = np.array(validation_image)
+#     validation_labal = np.array(validation_labal)
+#     test_image = np.array(test_image)
+#     test_labal = np.array(test_labal)
+#
+#     # Shuffle data.
+#     train_image, train_labal = Shuffle(train_image, train_labal)
+#     validation_image, validation_labal = Shuffle(validation_image, validation_labal)
+#     test_image, test_labal = Shuffle(test_image, test_labal)
+#
+#     # Save image.
+#     save_to_file(train_image,train_labal,validation_image,validation_labal,test_image,test_labal)
+#
+#     return train_image,train_labal,validation_image,validation_labal,test_image,test_labal
+#
+# # Load image from binary file.
+# def load_from_file():
+#
+#     if os.path.exists('FinalDataset/bin/train_image.npy') is True:
+#         train_image = np.load('FinalDataset/bin/train_image.npy')
+#         train_labal = np.load('FinalDataset/bin/train_labal.npy')
+#         validation_image = np.load('FinalDataset/bin/validation_image.npy')
+#         validation_labal = np.load('FinalDataset/bin/validation_labal.npy')
+#         test_image = np.load('FinalDataset/bin/test_image.npy')
+#         test_labal = np.load('FinalDataset/bin/test_labal.npy')
+#     else:
+#         train_image, train_labal, validation_image, validation_labal, test_image, test_labal = load_image()
+#
+#     # Return data.
+#     return train_image, train_labal, validation_image, validation_labal, test_image, test_labal
 
 # Save image to binary file.
 def save_to_file(train_image, train_labal, validation_image, validation_labal, test_image, test_labal):
@@ -122,3 +265,23 @@ def weight_variable(shape):
 def bias_variable(shape):
   initial = tf.constant(0.1, shape=shape)
   return tf.Variable(initial)
+
+def one_hot_to_label_array(one_hot):
+    label = []
+
+    for i in range(0,len(one_hot)):
+        label.append(np.where(one_hot[i]==1)[0][0])
+
+    return label
+
+def confusion_matrix1(actual, prediction):
+    cm = confusion_matrix(actual,prediction)
+
+    # df_cm = pd.DataFrame(cm, range(50),
+    #                      range(50))
+    # plt.figure(figsize = (50,40))
+    # sn.set(font_scale=1.4)  # for label size
+    # sn.heatmap(df_cm, annot=True, annot_kws={"size": 14})  # font size
+
+def Restore_and_run_test(model_name):
+    a = 1
